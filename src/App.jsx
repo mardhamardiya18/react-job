c15e5f05ada33e7f5ad77fadd8ae8140d3bd7fa2
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2024-03-24 22:52:28 +0800

    create detail job component with data API

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } 
 import React from 'react'
 import Homepage from './pages/Homepage'
 import MainLayout from './layouts/MainLayout'
-import JobPage from './pages/JobPage'
+import JobsPage from './pages/JobsPage'
 import NotFound from './pages/NotFound'
+import JobPage, { jobLoader } from './pages/JobPage'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<Homepage />} />
-      <Route path='/jobs' element={<JobPage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='/jobs/:id' element={< JobPage />} loader={jobLoader} />
       <Route path='*' element={<NotFound />} />
     </Route>
 
---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import JobList from '../components/JobList'
+
+const JobPage = () => {
+    return (
+        <section className=' bg-blue-50 py-5'>
+            <JobList />
+        </section>
+    )
+}
+
+export default JobPage
